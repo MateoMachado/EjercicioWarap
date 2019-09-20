3b9111fee98be5635bdb40efff0b3756991ddc56
Mateo Machado <mateo.machado@hotmail.com>
2019-09-20 18:42:35 -0300

    Program version1

--- PII_Phonebook_Start-master/src/Program/Program.cs
+++ PII_Phonebook_Start-master/src/Program/Program.cs
@@ -11,9 +11,14 @@ namespace Program
             owner dueño = new owner();
 
             // Crear la lista de contactos
-
+            contact contacto1 = new contact("Francisco");
+            contact contaco2 = new contact("Mateo");
+            contact contaco3 = new contact("Matias");
+            List<contact> contactos = new List<contact>();
+            
             // Agregar contactos a la lista
-
+            contactos.add(contacto2);
+            contactos.add(contacto3);
             // Enviar un correo a algunos contactos
 
             // Enviar un WhatsApp a algunos contactos
